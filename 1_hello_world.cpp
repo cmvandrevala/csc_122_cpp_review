f84626c43362e4869f082bf64a8f113e2553b75e
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2024-01-11 12:07:08 -0600

    Add 1_hello_world

---
+++ 1_hello_world.cpp
@@ -0,0 +1,15 @@
+// A bit of vocabulary:
+// The symbols // and /* */ specify comments
+// #include is a preprocessing directive
+// main is the entrypoint to the program that will be run
+// std:: is a namespace for functions defined in iostream
+// << is the stream insertion operator - it points towards where data should go
+// \n represents a newline. The backslash is called an escape character
+// return 0 exits the function successfully
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!\n";
+  return 0;
+}
